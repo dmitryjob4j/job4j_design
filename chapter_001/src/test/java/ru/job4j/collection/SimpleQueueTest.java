0e3dcd38add5bf1237481a3a3a96622775806baf
dmitryjob4j <haoos@inbox.ru>
2020-05-30 00:31:46 +0300

    5. Очередь на двух стеках [#288959]

--- chapter_001/src/test/java/ru/job4j/collection/SimpleQueueTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/SimpleQueueTest.java
@@ -18,8 +18,22 @@ import static org.hamcrest.core.Is.is;
  * @since 28.05.2020.
  */
 public class SimpleQueueTest<T> {
+
     @Test
-    public void test() {
+    public void testFromPArsentev() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+        assertThat(queue.poll(), is(1));
+        assertThat(queue.poll(), is(2));
+        queue.push(3);
+        queue.push(4);
+        assertThat(queue.poll(), is(3));
+        assertThat(queue.poll(), is(4));
+    }
+
+    @Test
+    public void test1() {
         SimpleQueue<Integer> queue = new SimpleQueue<>();
         queue.push(1);
         queue.push(2);
