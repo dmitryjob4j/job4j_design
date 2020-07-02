06ba9c5df0e460388beed0c910386c0b51c4217c
dmitryjob4j <haoos@inbox.ru>
2020-07-03 01:07:45 +0300

    2.Уровень-Джуниор.Блок.2.1.Ввод-вывод.4.0.File[#289007]

---
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+/**
+ * 2.Уровень-Джуниор.Блок.2..Ввод-вывод.1.Ввод-вывод
+ * 4.0. File [#289007]
+ *
+ * @since 03.07.2020
+ */
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:/projects/job4j_design");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Note exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            if (subfile.isFile()) {
+                System.out.println(subfile.getAbsoluteFile().getName()
+                        + " - "
+                        + subfile.getAbsoluteFile().length()
+                        + " byte");
+            }
+        }
+    }
+}
