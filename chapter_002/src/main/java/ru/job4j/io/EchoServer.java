89d198b560b423f03efb8aef1f3ac55a94fbe608
dmitryjob4j <haoos@inbox.ru>
2020-07-17 12:17:17 +0300

    2.Уровень-Джуниор.Блок.2.Ввод-вывод.2.Socket.0.Что такое Socket?[#289018]

---
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 2.Уровень-Джуниор.Блок.2.Ввод-вывод.2.Socket
+ * 0. Что такое Socket? [#289018]
+ *
+ * @since 17.07.2020
+ */
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            boolean work = true;
+            while (work) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str;
+                    //noinspection CheckStyle
+                    while (!(str = in.readLine()).isEmpty()) {
+                        System.out.println(str);
+                        if (str.contains("msg=Bye")) {
+                            work = false;
+                        }
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                }
+            }
+        }
+    }
+}
