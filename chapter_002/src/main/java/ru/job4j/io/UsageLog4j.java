4e91a747a6aedaa071b168a30310961923513042
dmitryjob4j <haoos@inbox.ru>
2020-07-18 23:22:56 +0300

    2.Уровень-Джуниор.Блок.2.Ввод-вывод.3.Логгирование. 2.Simple Loggin Facade 4 Java.[#288997]

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,16 +1,16 @@
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 2. Уровень - Джуниор.Блок.2.Ввод-вывод.3.Логгирование.
- * 1.Log4j.Логирование системы.[#288996]
+ * 2. Simple Loggin Facade 4 Java.[#288997]
  *
  * @since 18.07.2020
  */
 public class UsageLog4j {
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     /**
      * main
