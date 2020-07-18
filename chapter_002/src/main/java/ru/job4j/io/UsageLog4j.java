b66e39afa8d1aa1f6116022b41c19d26975cc844
dmitryjob4j <haoos@inbox.ru>
2020-07-18 23:41:09 +0300

    2.Уровень-Джуниор.Блок.2.Ввод-вывод.3.Логгирование. 3.Slf4j-вывод переменных.[#288998]

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 
 /**
  * 2. Уровень - Джуниор.Блок.2.Ввод-вывод.3.Логгирование.
- * 2. Simple Loggin Facade 4 Java.[#288997]
+ * 3.Slf4j-вывод переменных.[#288998]
  *
  * @since 18.07.2020
  */
 public class UsageLog4j {
+    /**
+     * Константа.
+     */
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     /**
@@ -23,5 +26,15 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        byte bate1 = 1;
+        short short2 = 2;
+        boolean boolean3 = true;
+        int int4 = 4;
+        char char5 = 'c';
+        long long6 = 6;
+        double double7 = 7.77;
+        float float8 = 888;
+        LOG.debug("byte: {}, short: {}, boolean: {}, int: {}, char: {}, long: {}, double: {}, float: {}",
+                bate1, short2, boolean3, int4, char5, long6, double7, float8);
     }
 }
