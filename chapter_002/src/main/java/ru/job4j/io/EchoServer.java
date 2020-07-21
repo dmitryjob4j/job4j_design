aa846391b26ebbc92f165f9f49eb1c19890c06b8
dmitryjob4j <haoos@inbox.ru>
2020-07-21 16:37:23 +0300

    2.Уровень-Джуниор.Блок.2.Ввод-вывод.2.Socket.1.4.Slf4j-вывод exception.[#288999]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -27,18 +27,17 @@ public class EchoServer {
      */
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
+            while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    str = in.readLine();
+                    String str = in.readLine();
                     System.out.println(str);
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     if (str.contains("msg=Exit")) {
                         out.write("Bye".getBytes());
-                        break;
+                        server.close();
                     } else if (str.contains("msg=Hello")) {
                         out.write("Hello, dear friend.".getBytes());
                     } else if (!(str.contains("msg=Exit") || str.contains("msg=Hello")) && str.contains("msg=")) {
