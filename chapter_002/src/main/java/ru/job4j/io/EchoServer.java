025b8a7e2536592c0e2e7dfab3abdd3df0597f42
dmitryjob4j <haoos@inbox.ru>
2020-07-21 13:49:39 +0300

    2.Уровень-Джуниор.Блок.2.Ввод-вывод.2.Socket.1.Бот[#289019]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -19,31 +19,23 @@ public class EchoServer {
      */
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            boolean work = true;
-            while (work) {
+            while (true) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
-                    String result;
-                    for (str = in.readLine(); !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains("msg=Exit")) {
-                            work = false;
-                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            out.write("Bye".getBytes());
-                            break;
-                        } else if (str.contains("msg=Hello")) {
-                            result = "Hello, dear friend";
-                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            out.write(result.getBytes());
-                        } else if (!(str.contains("msg=Exit") || str.contains("msg=Hello")) && str.contains("msg=")) {
-                            String[] tmp = str.split("[ |=]", 4);
-                            result = tmp[2];
-                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                            out.write(result.getBytes());
-                        }
+                    str = in.readLine();
+                    System.out.println(str);
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    if (str.contains("msg=Exit")) {
+                        out.write("Bye".getBytes());
+                        break;
+                    } else if (str.contains("msg=Hello")) {
+                        out.write("Hello, dear friend.".getBytes());
+                    } else if (!(str.contains("msg=Exit") || str.contains("msg=Hello")) && str.contains("msg=")) {
+                        String[] tmp = str.split("[ |=]", 4);
+                        out.write(tmp[2].getBytes());
                     }
                 }
             }
